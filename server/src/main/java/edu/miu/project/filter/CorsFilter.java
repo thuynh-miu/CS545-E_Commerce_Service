eed6919125166b5e910e1a35d413792cbfdf72c6
thuynh-miu <thuynh@miu.edu>
2024-11-21 22:33:57 -0600

    feat(ED-4): fix cors

--- server/src/main/java/edu/miu/project/filter/CorsFilter.java
+++ server/src/main/java/edu/miu/project/filter/CorsFilter.java
@@ -10,7 +10,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+//@Component
 public class CorsFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
