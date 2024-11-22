f7b3ede059421c395cc4e88dd006943b90c7644a
hsonthach <son.thach011011@gmail.com>
2024-11-21 20:17:53 -0600

    Integrate product show

--- client/src/components/ProductDetailPage/index.js
+++ client/src/components/ProductDetailPage/index.js
@@ -1,36 +1,32 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import AddToCartButton from "../AddToCartButton";
 import ProductReview from "../ProductReviews";
-import { Link } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import Rating from "react-rating";
 import { StarFilled, StarOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 export default function ProductDetailPage(props) {
-  const [productDetail, setProductDetail] = useState({
-    name: "Apple iPhone 15 Pro 128GB Black Titanium",
-    brand: "Apple",
-    rating: "4.3",
-    price: 1049.0,
-    stock_left: 10,
-    img_url:
-      "https://i5.walmartimages.com/seo/Apple-iPhone-15-Pro-128GB-Black-Titanium_9f6f8b16-d4ea-4ecc-86f8-114386e6381e.88e5b55ab4cd0b6325266a880c055f64.jpeg?odnHeight=2000&odnWidth=2000&odnBg=FFFFFF",
-    stock: 10,
-    reviews: [
-      {
-        author: "Cuong",
-        title: "It sucks!",
-        content: "Samsung makes better phones",
-        rating: 0,
-      },
-      {
-        author: "Ronaldo",
-        title: "I love it!",
-        content: "This is my 1000-th phone",
-        rating: 5,
-      },
-    ],
-    about: `<ul>   <li>FORGED IN TITANIUM - iPhone 15 Pro has a strong and light aerospace-grade titanium design with a textured matte-glass back. It also features a Ceramic Shield front that's tougher than any smartphone glass. And it's splash, water, and dust resistant.</li>   <li>ADVANCED DISPLAY - The 6.1" Super Retina XDR display with ProMotion ramps up refresh rates to 120Hz when you need exceptional graphics performance. Dynamic Island bubbles up alerts and Live Activities. Plus, with Always-On display, your Lock Screen stays glanceable, so you don't have to tap it to stay in the know.</li> </ul>`,
-  });
+  const [productDetail, setProductDetail] = useState(null);
+  // get id from http://localhost:3000/products/detail/2
+  const { productId } = useParams();
+
+  useEffect(() => {
+    // fetch product detail from API http://localhost:8080/api/v1/products/1
+    axios.get(`http://localhost:8080/api/v1/products/${productId}`).then((res) => {
+      setProductDetail({
+        id: res.data.id,
+        name: res.data.name,
+        description: res.data.description,
+        price: res.data.price,
+        quantity: res.data.quantity,
+        img_url: res.data.imageUrl,
+        reviews: res.data.reviews
+      });
+    });
+  }, []);
+
+
 
   const [addedQuantity, setAddedQuantity] = useState(0);
 
@@ -41,6 +37,13 @@ export default function ProductDetailPage(props) {
   const decrease = () => {
     setAddedQuantity((addedQuantity) => addedQuantity - 1);
   };
+
+  if (!productDetail){
+    return <>
+    </>
+  }
+
+  console.log(productDetail)
 
   return (
     <div className="container">
