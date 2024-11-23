e59bf6108204805a3e064e7ed0cfe81ef86b6ecf
Cường Nguyễn Ngọc <cuongnguyenngoc@Cuongs-MacBook-Pro-8.local>
2024-11-22 22:07:12 -0600

    Fix permission

--- client/src/components/BuyerDashboard/index.js
+++ client/src/components/BuyerDashboard/index.js
@@ -1,12 +1,14 @@
-import { Outlet } from "react-router-dom";
-
+import { Outlet, useNavigate } from "react-router-dom";
+import { useUserContext } from "../../contexts/UserContextProvider";
+import { UserRole } from "../../constants/UserRole";
 
 export default function BuyerDashboard(props) {
     // Check buyer permissions here
-
-    return (
-        <div className="container">
-            <Outlet />
-        </div>
-    )
+    const { userData } = useUserContext();
+    const navigate = useNavigate();
+    const isAuthorized = userData?.role === UserRole.BUYER;
+    if (!isAuthorized) {
+        navigate("/403");
+    }
+    return <div className="container">{isAuthorized && <Outlet />}</div>;
 }
